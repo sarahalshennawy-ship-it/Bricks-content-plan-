559fb3a9f01e3b74f1af2be7c7d6782990548027
sarahalshennawy-ship-it <sarahalshennawy@gmail.com>
2026-07-09 12:29:41 +0400

    Update issue-code.js

--- api/issue-code.js
+++ api/issue-code.js
@@ -24,6 +24,12 @@ async function getRedis() {
 }
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-admin-key');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'method_not_allowed' });
   }
